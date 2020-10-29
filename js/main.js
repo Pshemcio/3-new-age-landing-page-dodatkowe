a85fb4d91fcd9253e0d6758993891ac0b7604c36
Pshemcio <daglas92@gmail.com>
2020-10-29 12:14:56 +0100

    task complete

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', scrollEvents);
+
+
+});
+
+const scrollEvents = () => {
+    const showNavbar = () => {
+        (window.scrollY > document.querySelector('header').offsetHeight - 25 && window.innerWidth > 575) ? document.querySelector('.navbar').classList.add('bg-move'): document.querySelector('.navbar').classList.remove('bg-move');
+    };
+    showNavbar();
+
+    const headerParallax = () => {
+        document.querySelector('.mask').style.transform = `translateY(${window.scrollY/1.2}px)`;
+    };
+    headerParallax();
+};
